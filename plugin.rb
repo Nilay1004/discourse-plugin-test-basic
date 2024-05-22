6f8e5043f314a3ea68c07103953c2458e5abdef2
Nilay1004 <nilaykujur@gmail.com>
2024-05-22 12:34:58 +0530

    Shreyansh(required dependency user_email v1)

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,6 @@
 
 # name: discourse-plugin-name-nilay
 # about: Test
-# meta_topic_id: 123
 # version: 0.0.1
 # authors: Nilay
 # url: https://github.com/Nilay1004/discourse-plugin-test-basic
@@ -18,40 +17,39 @@ require_relative "lib/my_plugin_module/engine"
 
 after_initialize do
   Rails.logger.info "PIIEncryption: Plugin initialized"
-  require_dependency 'user'
+  require_dependency 'user_email'
 
   module ::PIIEncryption
     def self.encrypt_email(email)
-      # Log the email before and after encryption
-      Rails.logger.info "PIIEncryption: Original email: #{email}"
+      # Encryption logic here
       encrypted_email = email.reverse
-      Rails.logger.info "PIIEncryption: Encrypted email: #{encrypted_email}"
       encrypted_email
     end
 
     def self.decrypt_email(encrypted_email)
-      return nil if encrypted_email.nil?
-      Rails.logger.info "PIIEncryption: Decrypting email: #{encrypted_email}"
+      # Decryption logic here
       decrypted_email = encrypted_email.reverse
-      Rails.logger.info "PIIEncryption: Decrypted email: #{decrypted_email}"
       decrypted_email
     end
   end
 
   class ::UserEmail
-    before_save :encrypt_email_address
-    after_save :decrypt_email_address
+    before_save :encrypt_email_field
 
-    def encrypt_email_address
-      if self.email.present?
-       Rails.logger.info "PIIEncryption: Encrypting email for user:"
-       self.email = PIIEncryption.encrypt_email(read_attribute(:email))
-       self.save
-      end
+    def email
+      # Override to maintain the encrypted state
+      read_attribute(:email)
     end
 
-    def decrypt_email_address
-      @email=PIIEncryption.decrypt_email(read_attribute(:email))
+    def decrypted_email
+      # Method to get the decrypted email when necessary
+      PIIEncryption.decrypt_email(self.email)
+    end
+
+    private
+
+    def encrypt_email_field
+      self.email = PIIEncryption.encrypt_email(self.email)
     end
   end
 end
