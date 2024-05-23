385a3ab7354d51bf1b8d2412d0c91403d5e2ae79
Nilay1004 <nilaykujur@gmail.com>
2024-05-23 16:48:09 +0530

    Laststst.rb

--- plugin.rb
+++ plugin.rb
@@ -8,24 +8,28 @@
 # url: https://github.com/Nilay1004/discourse-plugin-test-basic
 # required_version: 2.7.0
 
-enabled_site_setting :reverse_email_login_enabled
+
 
 after_initialize do
   module ::ReverseEmailLogin
-    module DefaultCurrentUserProviderExtensions
-      def find_user_by_identifier(email_or_username)
-        if SiteSetting.reverse_email_login_enabled && email_or_username.include?("@")
-          email_or_username = email_or_username.reverse
-          Rails.logger.info "Reversed Email in CurrentUserProvider: #{email_or_username}"
+    module SessionControllerExtensions
+      def create
+        login = params[:login]&.strip
+        if login.present? && login.include?("@")
+          reversed_login = login.reverse
+          Rails.logger.info "Original Email: #{login}"
+          Rails.logger.info "Reversed Email: #{reversed_login}"
+          params[:login] = reversed_login
         end
-        super(email_or_username)
+
+        super # Call the original create method
       end
     end
   end
 
   # Ensure the module is loaded
-  require_dependency 'auth/default_current_user_provider'
+  require_dependency 'session_controller'
 
-  # Prepend the module
-  Auth::DefaultCurrentUserProvider.prepend ::ReverseEmailLogin::DefaultCurrentUserProviderExtensions
+  # Prepend the module to extend the SessionController
+  ::SessionController.prepend ::ReverseEmailLogin::SessionControllerExtensions
 end
