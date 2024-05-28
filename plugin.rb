a104c3d7744ca40d081ff69be33b405c96770d14
Nilay1004 <nilaykujur@gmail.com>
2024-05-28 09:54:06 +0530

    Update.rb

--- plugin.rb
+++ plugin.rb
@@ -8,46 +8,69 @@
 # url: https://github.com/Nilay1004/discourse-plugin-test-basic
 # required_version: 2.7.0
 
+enabled_site_setting :plugin_name_enabled
+
+module ::MyPluginModule
+  PLUGIN_NAME = "discourse-plugin-name-darshan"
+end
+
+require_relative "lib/my_plugin_module/engine"
+
 after_initialize do
-  module ::ReverseEmailLogin
-    class ReversedEmailAuthenticator < ::Auth::Authenticator
-      def name
-        'reversed_email'
-      end
+  Rails.logger.info "PIIEncryption: Plugin initialized"
+  require_dependency 'user_email'
 
-      def description
-        'Log in with your email address reversed'
-      end
-
-      def primary_email_verified?(auth_token)
-        true
-      end
-
-      def after_authenticate(auth_token)
-        result = Auth::Result.new
-
-        if auth_token[:email].present?
-          reversed_email = auth_token[:email].reverse
-          Rails.logger.info "Original Email: #{auth_token[:email]}"
-          Rails.logger.info "Reversed Email: #{reversed_email}"
-          user = User.find_by_email(reversed_email)
-
-          if user
-            result.user = user
-            result.email = user.email
-            result.email_valid = true
-          else
-            result.failed = true
-          end
-        else
-          result.failed = true
-        end
-
-        result
-      end
+  module ::PIIEncryption
+    def self.encrypt_email(email)
+      return email if email.nil? || email.empty?
+      Rails.logger.info "PIIEncryption: Encrypting email: #{email}"
+      encrypted_email = email.reverse # Simple encryption by reversing the string
+      Rails.logger.info "PIIEncryption: Encrypted email: #{encrypted_email}"
+      encrypted_email
     end
 
-    # Register the custom authenticator
-    Auth::Authenticator.register(ReversedEmailAuthenticator.new)
+    def self.decrypt_email(encrypted_email)
+      return encrypted_email if encrypted_email.nil? || encrypted_email.empty?
+      Rails.logger.info "PIIEncryption: Decrypting email: #{encrypted_email}"
+      decrypted_email = encrypted_email.reverse
+      Rails.logger.info "PIIEncryption: Decrypted email: #{decrypted_email}"
+      decrypted_email
+    end
   end
+
+  class ::UserEmail
+    before_save :encrypt_email_address
+
+    def email
+      @decrypted_email ||= PIIEncryption.decrypt_email(read_attribute(:email))
+    end
+
+    def email=(value)
+      @decrypted_email = value
+      write_attribute(:email, PIIEncryption.encrypt_email(value))
+    end
+
+    private
+
+    def encrypt_email_address
+      if email_changed?
+        write_attribute(:email, PIIEncryption.encrypt_email(@decrypted_email))
+      end
+    end
+  end
+
+  # Ensure we do not decrypt the email during validation
+  module ::PIIEncryption::UserPatch
+    def email
+      if new_record?
+        # Return the raw email attribute during the signup process
+        read_attribute(:email)
+      else
+        super
+      end
+    end
+  end
+
+  ::User.prepend(::PIIEncryption::UserPatch)
 end
+
